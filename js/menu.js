d5217a2a43d920ae828bbbfe3b865bd7e0f83abd
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-08-28 10:19:16 +0300

    added

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
       body: document.querySelector('[body]'),
-    menuList: document.querySelector('[mob-menu]'),
+    menuList: document.querySelector('[mob-menu__link]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
